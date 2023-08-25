f988b62352fe5b997e9e806dd8cc1bbcce13b511
Alumnos <Alumnos@SCL100PCLC30030.alumnos.cst>
2023-08-24 20:50:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/miaplicacion/loginApp.java
@@ -0,0 +1,14 @@
+package com.example.miaplicacion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class loginApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.mainlobby);
+    }
+
+}
